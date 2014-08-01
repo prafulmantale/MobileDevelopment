6af2695cd82b4a37025c19b5b4a0a97337a4dc41
Praful Mantale <praful_mantale@yahoo.com>
2014-08-01 00:19:29 -0700

    Updated model

--- AndroidApps/SimpleTodoList/app/src/main/java/prafulmantale/simpletodolist/models/ToDoItem.java
+++ AndroidApps/SimpleTodoList/app/src/main/java/prafulmantale/simpletodolist/models/ToDoItem.java
@@ -7,13 +7,40 @@ public class ToDoItem {
 
     private String item;
     private boolean isCompleted;
+    private boolean isSelected;
 
     public ToDoItem(){
         item = null;
         isCompleted = false;
+        isSelected = false;
     }
     public ToDoItem(String item) {
         this.item = item;
         isCompleted = false;
+        isSelected = false;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(boolean isSelected) {
+        this.isSelected = isSelected;
     }
 }
