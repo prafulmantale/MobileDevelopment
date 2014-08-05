e46e03fc5d3536aaac7c06b04b1ea93b90be28a9
Praful Mantale <praful_mantale@yahoo.com>
2014-08-04 18:14:55 -0700

    due date model changes

--- AndroidApps/SimpleTodoList/app/src/main/java/prafulmantale/simpletodolist/models/ToDoItem.java
+++ AndroidApps/SimpleTodoList/app/src/main/java/prafulmantale/simpletodolist/models/ToDoItem.java
@@ -1,5 +1,9 @@
 package prafulmantale.simpletodolist.models;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * Created by prafulmantale on 7/31/14.
  */
@@ -8,14 +12,20 @@ public class ToDoItem {
     private long id;
     private String item;
     private boolean isCompleted;
+    private boolean isDueDateConfigured;
+    private Date dueDate;
 
     public ToDoItem(){
         item = null;
         isCompleted = false;
+        isDueDateConfigured = false;
+        dueDate = new Date();
     }
     public ToDoItem(String item) {
         this.item = item;
         isCompleted = false;
+        isDueDateConfigured = false;
+        dueDate = new Date();
     }
 
     public long getId(){
@@ -40,6 +50,32 @@ public class ToDoItem {
 
     public void setCompleted(boolean isCompleted) {
         this.isCompleted = isCompleted;
+    }
+
+    public boolean isDueDateConfigured() {
+        return isDueDateConfigured;
+    }
+
+    public void setDueDateConfigured(boolean isDueDateConfigured) {
+        this.isDueDateConfigured = isDueDateConfigured;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getDateTime(){
+        if(!isDueDateConfigured){
+            return "";
+        }
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+
+        return simpleDateFormat.format(dueDate);
     }
 
     @Override
