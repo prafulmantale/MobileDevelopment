43b994f3a891b4aae1543265883d46c5073dc8b0
Praful Mantale <praful_mantale@yahoo.com>
2014-08-06 11:07:12 -0700

    display message app

---
+++ AndroidApps/DisplayMessageApplication/app/src/main/java/prafulmantale/displaymessageapplication/DisplayMessageActivity.java
@@ -0,0 +1,51 @@
+package prafulmantale.displaymessageapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import prafulmantale.displaymessageapplication.R;
+
+public class DisplayMessageActivity extends Activity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        textView = (TextView)findViewById(R.id.displaymessage);
+
+        Intent intent = getIntent();
+        if(intent.hasExtra((EnterMessageActivity.DISPLAY_MESSAGE))){
+            String message = intent.getStringExtra(EnterMessageActivity.DISPLAY_MESSAGE);
+            textView.setText(message);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.display_message, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ AndroidApps/DisplayMessageApplication/app/src/main/java/prafulmantale/displaymessageapplication/EnterMessageActivity.java
@@ -0,0 +1,52 @@
+package prafulmantale.displaymessageapplication;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class EnterMessageActivity extends Activity {
+
+    private EditText editText;
+    public static final String DISPLAY_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_message);
+
+        editText = (EditText)findViewById(R.id.edittext);
+    }
+
+    public void sendMessage(View view){
+
+        Intent intent = new Intent(EnterMessageActivity.this, DisplayMessageActivity.class);
+        intent.putExtra(DISPLAY_MESSAGE, editText.getText().toString());
+        startActivity(intent);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.enter_message, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
