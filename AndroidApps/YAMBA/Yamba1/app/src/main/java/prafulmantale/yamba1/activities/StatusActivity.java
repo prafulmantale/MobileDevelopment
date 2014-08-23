d4170ea3d76961e18c59de4d7bbca1d2d3f8c828
Praful Mantale <praful_mantale@yahoo.com>
2014-08-22 18:51:09 -0700

    yamba app

--- AndroidApps/YAMBA/Yamba1/app/src/main/java/prafulmantale/yamba1/activities/StatusActivity.java
+++ AndroidApps/YAMBA/Yamba1/app/src/main/java/prafulmantale/yamba1/activities/StatusActivity.java
@@ -2,20 +2,63 @@ package prafulmantale.yamba1.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import prafulmantale.yamba1.R;
 
 
 public class StatusActivity extends Activity {
 
+    private final static String TAG = "StatusActivity";
+    private Button updateButton;
+    private EditText editStatus;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status_activity);
+
+        initialize();
+
+        setupListeners();
     }
 
+    private void setupListeners() {
+
+//        updateButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String status = editStatus.getText().toString();
+//                if(status == null || status.isEmpty() || status.trim().isEmpty()) {
+//                    Log.d(TAG, "Update button clicked");
+//                }
+//                else{
+//                    Log.d(TAG, status);
+//                }
+//            }
+//        });
+    }
+
+    public void updateClicked(View view){
+        String status = editStatus.getText().toString();
+        if(status == null || status.isEmpty() || status.trim().isEmpty()) {
+            Log.d(TAG, "updateClicked - Update button clicked");
+        }
+        else{
+            Log.d(TAG, status);
+        }
+    }
+
+
+    private void initialize(){
+        updateButton = (Button)findViewById(R.id.updateButton);
+        editStatus = (EditText)findViewById(R.id.statusMessage);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
