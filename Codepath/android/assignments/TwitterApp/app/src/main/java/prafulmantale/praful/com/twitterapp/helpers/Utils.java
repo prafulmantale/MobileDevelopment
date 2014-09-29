fa2f2b1d939cadb2b8538c7e0c0821461712535a
Praful Mantale <praful_mantale@yahoo.com>
2014-09-28 19:16:23 -0700

    helper

--- Codepath/android/assignments/TwitterApp/app/src/main/java/prafulmantale/praful/com/twitterapp/helpers/Utils.java
+++ Codepath/android/assignments/TwitterApp/app/src/main/java/prafulmantale/praful/com/twitterapp/helpers/Utils.java
@@ -1,6 +1,10 @@
 package prafulmantale.praful.com.twitterapp.helpers;
 
 import java.net.URLConnection;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by prafulmantale on 9/26/14.
@@ -29,5 +33,35 @@ public class Utils {
 //        return false;
 
         return isAvailable;
+    }
+
+    public static String getElapsedDisplayTime(long createdTime){
+        String display = "";
+
+        Date date = new Date();
+        date.setTime(createdTime * 1000);
+
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        Date nowDate = calendar1.getTime();
+
+        long diff = nowDate.getTime() - date.getTime();
+
+        long hrDiff = TimeUnit.MILLISECONDS.toHours(diff);
+        long minDiff = TimeUnit.MILLISECONDS.toMinutes(diff) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(diff));
+        if(hrDiff > 0){
+            display = String.format("%dh", hrDiff);
+        }
+        else{
+            if(minDiff < 0){
+                minDiff = 0;
+            }
+
+            display = String.format("%dm", minDiff);
+        }
+
+
+        return display;
     }
 }
