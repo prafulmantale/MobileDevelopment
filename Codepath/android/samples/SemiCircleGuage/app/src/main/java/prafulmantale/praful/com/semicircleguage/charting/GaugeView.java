e2c314da0400c4397aa0a8768461ce367be45f01
Praful Mantale <praful_mantale@yahoo.com>
2014-11-06 11:46:11 -0800

    updated

--- Codepath/android/samples/SemiCircleGuage/app/src/main/java/prafulmantale/praful/com/semicircleguage/charting/GaugeView.java
+++ Codepath/android/samples/SemiCircleGuage/app/src/main/java/prafulmantale/praful/com/semicircleguage/charting/GaugeView.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
+import android.graphics.Region;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -19,6 +23,10 @@ import prafulmantale.praful.com.semicircleguage.R;
 public class GaugeView extends View {
     private Bitmap bgImage = null;
     private Bitmap bgImage2 = null;
+    private Bitmap posNone = null;
+    private Bitmap posShort = null;
+    private Bitmap posLong = null;
+
 
     float w;
     float h;
@@ -64,11 +72,16 @@ public class GaugeView extends View {
 
         bgImage = BitmapFactory.decodeResource(getResources(), R.drawable.red_guage);
         bgImage2 = BitmapFactory.decodeResource(getResources(), R.drawable.blue_guage);
+
+        posNone = BitmapFactory.decodeResource(getResources(), R.drawable.position_none172);
+        posShort = BitmapFactory.decodeResource(getResources(), R.drawable.position_short172);
+        posLong = BitmapFactory.decodeResource(getResources(), R.drawable.position_long172);
+
         w = bgImage.getWidth();
         h = bgImage.getHeight();
 
         arcPaint = new Paint();
-        arcPaint.setStyle(Paint.Style.STROKE);
+        arcPaint.setStyle(Paint.Style.FILL);
         arcPaint.setStrokeWidth(2.5f * getResources().getDisplayMetrics().density);
         arcPaint.setColor(getResources().getColor(R.color.arcsecondarycolor));
         arcPaint.setAntiAlias(true);
@@ -96,10 +109,45 @@ public class GaugeView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawBitmap(bgImage, 0, 0, null);
-        canvas.drawBitmap(bgImage2, w, w, null);
-        canvas.drawArc(mOval, startAngle, 180, false, arcPaint);
+//        canvas.drawBitmap(bgImage, 0, 0, null);
+//        canvas.drawBitmap(bgImage2, w, w, null);
+//        canvas.drawArc(mOval, startAngle, 180, false, arcPaint);
+//
+//       canvas.drawPath(needlePath, needlePaint);
 
-       canvas.drawPath(needlePath, needlePaint);
+
+        //
+        //canvas.drawBitmap(posLong, posLong.getWidth(), 0, null);
+
+        canvas.drawBitmap(posNone, 0, 0, null);
+//        //canvas.drawBitmap(posShort, 0, 0, null);
+        canvas.drawBitmap(posNone, posNone.getWidth(), 0, null);
+//
+//        Rect source = new Rect(0, 0, posNone.getWidth(), posNone.getHeight());
+//        RectF bitmapRect = new RectF(0, 0,  posNone.getWidth() - 20,posNone.getHeight());
+//        canvas.drawBitmap(cropBitmap1(), source, bitmapRect, null);
+//        //canvas.drawRect(bitmapRect, arcPaint);
+
+        canvas.clipRect(0, 0, canvas.getWidth(), canvas.getHeight(), Region.Op.REPLACE);
+        arcPaint.setColor(Color.WHITE);
+        arcPaint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), arcPaint);
+
+        canvas.clipRect(0, 0, 0 + 100, 0, Region.Op.REPLACE);
+        canvas.drawBitmap(posShort, 0 , 0, arcPaint);
+    }
+
+    private Bitmap cropBitmap1()
+    {
+        Bitmap bmp2 = BitmapFactory.decodeResource(this.getResources(), R.drawable.position_short172);
+        Bitmap bmOverlay = Bitmap.createBitmap(320, 480, Bitmap.Config.ARGB_8888);
+
+        Paint p = new Paint();
+        p.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        Canvas c = new Canvas(bmOverlay);
+        c.drawBitmap(bmp2, 0, 0, null);
+        c.drawRect(30, 30, 100, 100, p);
+
+        return bmOverlay;
     }
 }
