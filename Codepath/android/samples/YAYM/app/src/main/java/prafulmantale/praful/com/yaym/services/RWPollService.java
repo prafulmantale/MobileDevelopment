090373d6b615f631f5de9b98ab45515e3b9fb876
Praful Mantale <praful_mantale@yahoo.com>
2014-11-16 11:34:36 -0800

    updated

--- Codepath/android/samples/YAYM/app/src/main/java/prafulmantale/praful/com/yaym/services/RWPollService.java
+++ Codepath/android/samples/YAYM/app/src/main/java/prafulmantale/praful/com/yaym/services/RWPollService.java
@@ -12,14 +12,17 @@ import android.util.Log;
 public class RWPollService extends Service {
 
     private static final String TAG = RWPollService.class.getSimpleName();
+    private static final long POLL_FREQUENCY = 3000;
 
     private Looper looper;
+    private Poller poller;
+    private boolean isPolling = false;
 
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        poller = new Poller();
         Log.d(TAG, "OnCreated");
     }
 
@@ -33,17 +36,50 @@ public class RWPollService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "onStartCommand");
+        synchronized (this) {
+            if (isPolling == false) {
+                isPolling = true;
+                poller.start();
+            }
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy");
+        synchronized (this) {
+            Log.d(TAG, "onDestroy");
+            poller.interrupt();
+            isPolling = false;
+            poller = null;
+        }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    class Poller extends Thread{
+
+        public Poller(){
+            super("Poller");
+        }
+
+        @Override
+        public void run() {
+
+            while(isPolling) {
+                Log.d(TAG, "Poller.run");
+
+                try {
+                    sleep(POLL_FREQUENCY);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    isPolling = false;
+                }
+            }
+        }
     }
 }
