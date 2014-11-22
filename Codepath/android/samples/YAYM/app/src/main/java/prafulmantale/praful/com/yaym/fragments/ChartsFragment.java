41bf2e9511456de148582975290dc8f8c3e4de6d
Praful Mantale <praful_mantale@yahoo.com>
2014-11-22 14:17:48 -0800

    Charts fragment

---
+++ Codepath/android/samples/YAYM/app/src/main/java/prafulmantale/praful/com/yaym/fragments/ChartsFragment.java
@@ -0,0 +1,28 @@
+package prafulmantale.praful.com.yaym.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import prafulmantale.praful.com.yaym.R;
+
+/**
+ * Created by prafulmantale on 11/22/14.
+ */
+public class ChartsFragment extends Fragment{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_charts, container, false);
+
+        return view;
+    }
+}
--- Codepath/android/samples/YAYM/app/src/main/java/prafulmantale/praful/com/yaym/widgets/GaugeViewEx.java
+++ Codepath/android/samples/YAYM/app/src/main/java/prafulmantale/praful/com/yaym/widgets/GaugeViewEx.java
@@ -2,11 +2,8 @@ package prafulmantale.praful.com.yaym.widgets;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -22,11 +19,9 @@ public class GaugeViewEx extends View {
     private Paint outerCirclePaintBlank;
     private Paint outerCirclePaintProfit;
     private Paint outerCirclePaintLoss;
-    private Paint innerCirclePaint;
     private Paint meterLinePaint;
 
     private RectF outerRect;
-    private RectF innerRect;
     private RectF meterRect;
 
     private Paint needlePaint;
@@ -78,32 +73,26 @@ public class GaugeViewEx extends View {
         float newangle = (float)(angle * Math.PI / 180);
         float startX = center;
         float startY = 0.79f* height - 1;
-        float endX   = startX + (float)(40 * Math.sin(newangle));
-        float endY   = startY + (float)(40 * Math.cos(newangle));
 
+        float endX   = startX + (float)(meterRect.height()/2 * Math.sin(newangle));
+        float endY   = startY + (float)(meterRect.height()/2 * Math.cos(newangle));
 
         //Outer rect
         if(currentPnL >= 0) {
-            canvas.drawArc(outerRect, 180, 60, true, outerCirclePaintBlank);
-            canvas.drawArc(outerRect, 240, 120, true, outerCirclePaintProfit);
+            canvas.drawArc(outerRect, 180, 60, false, outerCirclePaintBlank);
+            canvas.drawArc(outerRect, 240, 120, false, outerCirclePaintProfit);
         }
         else{
             canvas.drawArc(outerRect, 180, 60, true, outerCirclePaintLoss);
             canvas.drawArc(outerRect, 240, 120, true, outerCirclePaintBlank);
         }
 
-        //Inner Rect
-        canvas.drawArc(innerRect, 180, 180, true, innerCirclePaint);
-
         //Meter line
         canvas.drawArc(meterRect, 180, 180, false, meterLinePaint);
-
-//        canvas.drawLine(innerRect.left, innerRect.bottom, innerRect.right, innerRect.bottom, outerCirclePaintLoss);
 
         //Needle
         canvas.drawCircle(startX, startY, 4, needlePaint);
         canvas.drawLine(startX, startY, endX, endY, needlePaint);
-
 
         canvas.drawText(lossThresholdText, outerRect.left - 5, height - height/6, textPaintLoss);
         canvas.drawText(profitThresholdText, outerRect.right + 5, height - height/6, textPaintProfit);
@@ -120,20 +109,18 @@ public class GaugeViewEx extends View {
 
         outerCirclePaintBlank = new Paint(Paint.ANTI_ALIAS_FLAG);
         outerCirclePaintBlank.setColor(getResources().getColor(R.color.pnl_blank));
-        outerCirclePaintBlank.setStyle(Paint.Style.FILL);
+        outerCirclePaintBlank.setStyle(Paint.Style.STROKE);
+        outerCirclePaintBlank.setStrokeWidth(meterWidth);
 
         outerCirclePaintProfit = new Paint(Paint.ANTI_ALIAS_FLAG);
         outerCirclePaintProfit.setColor(getResources().getColor(R.color.pnl_profit));
-        outerCirclePaintProfit.setStyle(Paint.Style.FILL);
+        outerCirclePaintProfit.setStyle(Paint.Style.STROKE);
+        outerCirclePaintProfit.setStrokeWidth(meterWidth);
 
         outerCirclePaintLoss = new Paint(Paint.ANTI_ALIAS_FLAG);
         outerCirclePaintLoss.setColor(getResources().getColor(R.color.pnl_loss));
-        outerCirclePaintLoss.setStyle(Paint.Style.FILL);
-
-        innerCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        innerCirclePaint.setColor(Color.WHITE);
-        innerCirclePaint.setShader(new LinearGradient(0, 0, 0, getHeight(), Color.parseColor("#f6f6f6"), Color.parseColor("#ededed"), Shader.TileMode.REPEAT));
-        innerCirclePaint.setStyle(Paint.Style.FILL);
+        outerCirclePaintLoss.setStyle(Paint.Style.STROKE);
+        outerCirclePaintLoss.setStrokeWidth(meterWidth);
 
         meterLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         meterLinePaint.setColor(getResources().getColor(R.color.pnl_meter_line));
@@ -167,7 +154,6 @@ public class GaugeViewEx extends View {
         center = width/2;
 
         outerRect = new RectF(outerRectLeft, outerRectTop, outerRectRight, outerRectHeight);
-        innerRect = new RectF(outerRectLeft + meterWidth, outerRectTop + meterWidth, outerRectRight - meterWidth, outerRectHeight - meterWidth);
         meterRect = new RectF(outerRectLeft +innerWidth, outerRectTop + innerWidth, outerRectRight - innerWidth, outerRectHeight - innerWidth);
 
         currentPnLText = "0.0";
@@ -184,7 +170,6 @@ public class GaugeViewEx extends View {
 
         setMeasuredDimension(width, height);
 
-
         init();
     }
 
@@ -200,7 +185,6 @@ public class GaugeViewEx extends View {
     }
 
     public void setCurrentPnL(double currentPnL, String currentPnLText) {
-//        if(this.currentPnL != currentPnL) {
             this.currentPnL = currentPnL;
             if(this.currentPnL >= 0) {
                 this.currentPnLText = currentPnLText;
