c4570ad7aa4245967d36aa2d69080561f4f43fdf
Praful Mantale <praful_mantale@yahoo.com>
2014-11-23 23:08:30 -0800

    ui refactoring

--- Codepath/android/samples/YAYM/app/src/main/java/prafulmantale/praful/com/yaym/activities/YieldMangerActivity.java
+++ Codepath/android/samples/YAYM/app/src/main/java/prafulmantale/praful/com/yaym/activities/YieldMangerActivity.java
@@ -11,7 +11,6 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.text.Html;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
