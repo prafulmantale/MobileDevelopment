b11bdfc33da5a1833d2151f3742822770a98d4e0
Praful Mantale <praful_mantale@yahoo.com>
2014-11-27 15:28:42 -0800

    updated

---
+++ IOSApps/rottenex/rottenex/MoviesViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MoviesViewController.swift
+//  rottenex
+//
+//  Created by Praful Mantale on 11/26/14.
+//  Copyright (c) 2014 prafulkumarmantale. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell") as MovieCell;
+        
+        cell.topLabel.text = "I am at row: \(indexPath.row), section \(indexPath.section)"
+   
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
