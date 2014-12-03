110b22dd798536d1c05396eadd6b38dad5df1d1e
Praful Mantale <praful_mantale@yahoo.com>
2014-12-03 11:54:45 -0800

    Rate properties

---
+++ IOSApps/loginscreen/loginscreen/RateProperties.swift
@@ -0,0 +1,23 @@
+//
+//  RateProperties.swift
+//  loginscreen
+//
+//  Created by Praful Mantale on 12/3/14.
+//  Copyright (c) 2014 prafulkumarmantale. All rights reserved.
+//
+
+import Foundation
+
+class RateProperties {
+    
+    var rate : Double = 0
+    var bigFigure : Double = 0
+    var pips : Double = 0
+    var fractionalPips : Double = 0
+    var onePipValue : Double = 0
+    
+    var rateDisplay : String = ""
+    var bigFigureDisplay : String = ""
+    var pipsDisplay : String = ""
+    var fractionalPipsDisplay : String = ""
+}
