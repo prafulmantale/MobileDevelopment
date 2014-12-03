3841f706a8f3e126420acd37f55153f43fe3a306
Praful Mantale <praful_mantale@yahoo.com>
2014-12-03 12:06:00 -0800

    ohlc and historical yield data

---
+++ IOSApps/loginscreen/loginscreen/HistoricYieldData.swift
@@ -0,0 +1,18 @@
+//
+//  HistoricYieldData.swift
+//  loginscreen
+//
+//  Created by Praful Mantale on 12/3/14.
+//  Copyright (c) 2014 prafulkumarmantale. All rights reserved.
+//
+
+import Foundation
+
+class HistoricYieldData{
+
+    var donePnL : Double = 0
+    var currentPnL : Double = 0
+    var doneVolume : Double = 0
+    var currentYield : Double = 0
+    var timestamp : Int64 = 0
+}
---
+++ IOSApps/loginscreen/loginscreen/OHLCData.swift
@@ -0,0 +1,18 @@
+//
+//  OHLCData.swift
+//  loginscreen
+//
+//  Created by Praful Mantale on 12/3/14.
+//  Copyright (c) 2014 prafulkumarmantale. All rights reserved.
+//
+
+import Foundation
+
+class OHLCData {
+    
+    var timestamp : Int64 = 0
+    var open : Double = 0
+    var close : Double = 0
+    var high : Double = 0
+    var low : Double = 0
+}
