11b5d3293fb5186efdd251e6ebe57133f1f89878
Praful Mantale <praful_mantale@yahoo.com>
2014-12-03 11:49:25 -0800

    reference data class

---
+++ IOSApps/loginscreen/loginscreen/ReferenceData.swift
@@ -0,0 +1,19 @@
+//
+//  ReferenceData.swift
+//  loginscreen
+//
+//  Created by Praful Mantale on 12/3/14.
+//  Copyright (c) 2014 prafulkumarmantale. All rights reserved.
+//
+
+import Foundation
+
+class ReferenceData{
+    
+    var instrument : String = ""
+    var spotPrecision : Int = 0
+    var spotPointsPrecision : Int = 0
+    var forwardPointsPrecision : Int = 0
+    var pipsFactor : Double = 0
+    var spotValueDate : String = ""
+}
