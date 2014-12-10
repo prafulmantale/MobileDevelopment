17c712f71486af1ddd1d1c062175a70deec962b6
Praful Mantale <praful_mantale@yahoo.com>
2014-12-10 10:06:28 -0800

    updated

--- Codepath/android/samples/YAYM/libs/AndroidBootstrap/build/generated/source/buildConfig/release/com/beardedhen/androidbootstrap/BuildConfig.java
+++ Codepath/android/samples/YAYM/libs/AndroidBootstrap/build/generated/source/buildConfig/release/com/beardedhen/androidbootstrap/BuildConfig.java
@@ -5,7 +5,7 @@ package com.beardedhen.androidbootstrap;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String PACKAGE_NAME = "com.beardedhen.androidbootstrap";
+  public static final String APPLICATION_ID = "com.beardedhen.androidbootstrap";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
