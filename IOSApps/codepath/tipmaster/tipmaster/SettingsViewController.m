9477cfb11487a14e2fc1976f18ca3089e7f53e9b
Praful Mantale <praful_mantale@yahoo.com>
2014-12-25 13:52:05 -0800

    settings

--- IOSApps/codepath/tipmaster/tipmaster/SettingsViewController.m
+++ IOSApps/codepath/tipmaster/tipmaster/SettingsViewController.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_gr.jpg"]];
 }
 
 - (void)didReceiveMemoryWarning {
