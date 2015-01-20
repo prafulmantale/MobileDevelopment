57ef59819a64b769fe4fa59f700a77b4e419063a
Praful Mantale <praful_mantale@yahoo.com>
2015-01-20 13:59:31 -0800

    grid health

---
+++ AndroidApps/Samples/Xerotouch/app/src/main/java/prafulmantale/praful/com/xerotouch/Fragment3.java
@@ -0,0 +1,20 @@
+package prafulmantale.praful.com.xerotouch;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment3 extends android.support.v4.app.Fragment {
+
+    public Fragment3() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_three, container, false);
+    }
+}
