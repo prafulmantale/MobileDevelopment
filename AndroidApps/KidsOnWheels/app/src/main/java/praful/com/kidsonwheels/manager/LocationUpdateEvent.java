958366901e7343b4fcca5288b402ced27eb22ed3
Praful Mantale <praful_mantale@yahoo.com>
2015-05-26 00:30:44 -0700

    LocationUpdateEvent

--- AndroidApps/KidsOnWheels/app/src/main/java/praful/com/kidsonwheels/manager/LocationUpdateEvent.java
+++ AndroidApps/KidsOnWheels/app/src/main/java/praful/com/kidsonwheels/manager/LocationUpdateEvent.java
@@ -1,14 +1,22 @@
 package praful.com.kidsonwheels.manager;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by prafulmantale on 5/26/15.
  */
 public class LocationUpdateEvent {
-    private double mLatitude;
-    private double mLongitude;
+    private LatLng mLatLang;
 
     public LocationUpdateEvent(double latitude, double longitude){
-        mLatitude = latitude;
-        mLongitude = longitude;
+        mLatLang = new LatLng(latitude, longitude);
+    }
+
+    public LocationUpdateEvent(LatLng latLng){
+        mLatLang = latLng;
+    }
+
+    public LatLng getLatLang(){
+        return mLatLang;
     }
 }
